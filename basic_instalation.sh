abd49741d19aee898e6941ebf6f99739dc75d952
lucasalmn <lucasalmrn@gmail.com>
2022-04-15 23:36:59 -0300

    implementation

--- basic_instalation.sh
+++ basic_instalation.sh
@@ -4,33 +4,33 @@
 
 echo '-- Started the instalation -- '
 
-sudo pacman -S zsh			\
+sudo pacman -S zsh				\
 	neofetch				\
-	vim						\
-	lxappearance			\
+	vim					\
+	lxappearance				\
 	nitrogen				\	
 	firefox					\
 	terminator				\
 	zathura					\
-	zathura-pdf-poppler		\
-	zathura-djvu			\
+	zathura-pdf-poppler			\
+	zathura-djvu				\
 	zathura-ps				\
-	git						\
-	tar						\
+	git					\
+	tar					\
 	unzip					\
 	p7zip					\
 	pcmanfm					\
 	gvfs 					\
 	gvfs-afc 				\
-	gvfs-gphoto2 			\
+	gvfs-gphoto2 				\
 	gvfs-mtp 				\
 	gvfs-mtp 				\
 	gvfs-smb				\
-	bash-completion			\
-	papirus-icon-theme		\
+	bash-completion				\
+	papirus-icon-theme			\
 	pavucontrol				\
 	pulseaudio				\
-	
+	flameshot					
 
 echo '--------------Done!------------------'
 echo '----Make good use of your computer!!!----'
