cf05087afbd5d549080a0f8451d757a26bd3721d
lucasalmn <lucasalmrn@gmail.com>
2022-04-20 11:55:50 -0300

    include configuration for vim

---
+++ config_vim.sh
@@ -0,0 +1,19 @@
+!/usr/bin/env
+mkdir -r ~/.vim/pack/git-plugins/start
+cd ~/.vim/pack/git-plugins/start
+echo 'para setar um plano de fundo'
+git clone https://github.com/rafi/awesome-vim-colorschemes.git
+echo 'para colocar a linha mostrando onde abre e onde fecha um scopo'
+git clone https://github.com/Yggdroot/indentLine 
+echo 'clonando navegador de arquivos'
+git clone https://github.com/preservim/nerdtree
+echo 'clonando icones para as pastas'
+git clone https://github.com/ryanoasis/vim-devicons
+echo 'clonando fote'
+cd ~/
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete%20Mono.otf
+mkdir ~/.fonts
+mv Droid\ Sans\ Mono\ Nerd\ Font\ Complete\ Mono.otf .fonts
+fc-cache ~/.fonts
+
+echo '----Done!!----'
